781d39595abefa53de168a32714bd9c8c114490b
ack <ack@DESKTOP-UCCH0VD>
2019-05-26 01:42:44 +0800

    ..

--- utils/util.js
+++ utils/util.js
@@ -570,7 +570,7 @@ function countDown(that, pageArray) {
 
 
 //全局-常量、变量 ###########################################################
-const apiHost = "http://127.0.0.1:9660/pinb-service"
+const apiHost = "https://apitest.pinb.vip/pinb-service"
 //https://apitest.pinb.vip/pinb-service
 //http://127.0.0.1:9660/pinb-service
 const appid = 'wx71de1973104f41cf'
